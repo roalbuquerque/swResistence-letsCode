2c1de646beb8bc943b5205948b4f2fb0c6cf8d14
Rafael Albuquerque <rafae.albuquerque.it@gmail.com>
2021-09-17 20:46:35 -0300

    Project created

---
+++ backend/src/main/java/com/letscode/swResistence/SwResistenceApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.swResistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwResistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwResistenceApplication.class, args);
+	}
+
+}
